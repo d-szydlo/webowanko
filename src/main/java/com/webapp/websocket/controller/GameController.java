4a11346a31fbdaad3dac9525c1b157a2997df20a
Dominika Szydlo <dszydlo28@gmail.com>
2020-01-28 20:52:01 +0100

    trochę do przodu

--- src/main/java/com/webapp/websocket/controller/GameController.java
+++ src/main/java/com/webapp/websocket/controller/GameController.java
@@ -1,28 +1,41 @@
 package com.webapp.websocket.controller;
 
 import com.webapp.websocket.model.GameMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class GameController {
 
-    @MessageMapping("/chat.sendMessage")
+    int playerCounter = 0;
+    @Autowired
+    private SimpMessagingTemplate template;
+
+    @MessageMapping("/game.sendMessage")
     @SendTo("/topic/public")
     public GameMessage sendMessage(@Payload GameMessage gameMessage) {
         return gameMessage;
     }
 
-    @MessageMapping("/chat.addUser")
-    @SendTo("/topic/public")
-    public GameMessage addUser(@Payload GameMessage chatMessage,
-                               SimpMessageHeaderAccessor headerAccessor) {
-        // Add username in web socket session
-        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
-        return chatMessage;
+    @MessageMapping("/game.addPlayer")
+    @SendTo("/topic/color")
+    public GameMessage addUser() {
+        GameMessage msg = new GameMessage();
+        if (playerCounter == 0){
+            msg.setSender("black");
+        } else {
+            msg.setSender("white");
+        }
+        playerCounter++;
+        return msg;
+    }
+
+    public void removeStone (String id){
+        template.convertAndSend("/topic/remove", id);
     }
 
 }
